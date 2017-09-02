8ee258e5ef4f2f096e24af7c70dd4ae676ee46fd
Yohan Rizk <yohan.rizk@gmail.com>
2017-09-02 14:34:06 -0400

    getting the xs to show up on the board

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 class Square extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			value: null,
+		};
+	}
 	render() {
 		return (
-			<button className="square">
-			{this.props.value}
+			<button className="square" onClick={() => this.setState({value: 'X'})} >
+			{this.state.value}
 			</button>
 			); 
 	}
