db8798d320dd338b8ab7e6abc11f8a7f0eb0d04a
Yohan Rizk <yohan.rizk@gmail.com>
2017-09-03 11:45:43 -0400

    game is functional.

--- src/index.js
+++ src/index.js
@@ -4,29 +4,51 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-class Square extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			value: null,
-		};
-	}
-	render() {
-		return (
-			<button className="square" onClick={() => this.setState({value: 'X'})} >
-			{this.state.value}
-			</button>
-			); 
-	}
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	);	
 }
 
 class Board extends React.Component {
-	renderSquare(i) {
-		return <Square value={i} /> ;
+	constructor() {
+		super();
+		this.state = {
+			squares : Array(9).fill(null),
+			xIsNext : true,
+		}
 	}
-	
+
+	renderSquare(i) {
+		return (
+				<Square value={this.state.squares[i]} 
+					   onClick={() => this.handleClick(i)}
+				/>
+		);
+	}
+
+	handleClick(i) {
+		const squares = this.state.squares.slice();
+		if (calculateWinner(squares) || squares[i]) {
+			return;
+		}
+		squares[i] = this.state.xIsNext ? 'X' : 'O';
+		this.setState({
+				squares : squares,
+				xIsNext : !this.state.xIsNext,
+		});
+	}
+
 	render() {
-		const status = 'Next Player: X';
+	   const winner = calculateWinner(this.state.squares)
+	   let status;
+	   if (winner) {
+		status = 'Winner is ' + winner;
+	   } else {
+	   	status = 'Next Player : ' +  (this.state.xIsNext ? 'X' : 'O');
+	   }
 
 		return (
 			<div>
@@ -66,5 +88,28 @@ class Game extends React.Component {
 		);
 	}
 }
+
+function calculateWinner(squares) {
+	// enumeration of all possible winning positions
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+}
+
 ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
